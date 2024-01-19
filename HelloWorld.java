a9b904a1c0ec6e30906647120960a22a5149a67b
Gajanan <105433300+ErGajanan@users.noreply.github.com>
2024-01-19 18:09:45 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld
         System.out.println("Hello World");
         System.out.println("This is 1224 branch code added by gajanan");
         System.out.println("Change code of master");
+        System.out.println("master changed");
     }
 }
